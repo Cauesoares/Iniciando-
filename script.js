d59111511921443d47a2812cecfcd320194d55cc
CAUe <caue.25@hotmail.com>
2023-09-21 19:50:28 -0300

    alteraçoes nas imagens

--- script.js
+++ script.js
@@ -8,10 +8,8 @@ function togglemode() {
   if (html.classList.contains("ligth")) {
     //se tiver ligth mode, adcionar a imagem ligth
     img.setAttribute("src", "./assets/avatar-ligth.png")
-    img.setAttribute("alt", "foto de caue")
   } else {
     //Se tiver sem o ligth mode , manter a imagem normal
-    img.setAttribute("src", "./assets/avaar.png")
-    img.setAttribute("alt", "a")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
