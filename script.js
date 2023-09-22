57cbb7997883097c9d95e789c1b4e2b7bece09e5
CAUe <caue.25@hotmail.com>
2023-09-22 19:46:41 -0300

    erro

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function togglemode() {
   const html = document.documentElement
   html.classList.toggle("ligth")
-
-  //Pegar a tag img
+  // pegar a tag img
   const img = document.querySelector("#profile img")
-  //substituir a imagem
+  // substituir a imagem
   if (html.classList.contains("ligth")) {
-    //se tiver ligth mode, adcionar a imagem ligth
+    // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-ligth.png")
   } else {
     //Se tiver sem o ligth mode , manter a imagem normal
